de50e1a983f088fd0332487d3bcef87ef9712327
sekoudosso82 <sekou.dosso82@gmail.com>
2020-02-17 20:56:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-
+    get '/' do
+        erb :index
+      end
+    # get '/' do
+    #     "<p>97 Bowling Alley Lane, Boulder CO </p>"
+    #     # "<h2>Bet You Can't Bowl A Strike Every Time</h2>"
+    #     # "<h1>Buster's Best Bowling Alley</h1>"
+    #     # "Buster's Best Bowling Alley"
+    #   end
+      
 end
